d8aaeb057f92152151c0c9fbb1b4616fdc257c90
mashu <mashu128a@126.com>
2014-01-26 11:08:55 +0800

    v2.0

--- master.py
+++ master.py
@@ -7,6 +7,9 @@ import time
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 from config_local import *
 import platfm
@@ -17,151 +20,169 @@ import term
 
 
 MixFlag = False  # 混合模式
-Depth=1  # 页面的嵌套深度
+Depth = 1  # 页面的嵌套深度
 
-def control(driver,sizeIdx):
-	'''在一页上处理用户按键'''
-	dts=driver.find_elements_by_class_name('pro_list_pic')
-	platfm.roll_down(driver,dts)
 
-	if AutoPickMode:
-		auto.auto_pick(driver,sizeIdx,dts)
-	else:
-		term.own_pick(driver,sizeIdx,dts)
+def control(driver, sizeIdx):
+    '''在一页上处理用户按键'''
+    dts = driver.find_elements_by_class_name('pro_list_pic')
+    platfm.roll_down(driver, dts)
+    print 'after roll down'
+    if AutoPickMode:
+        auto.auto_pick(driver, sizeIdx, dts)
+    else:
+        term.own_pick(driver, sizeIdx, dts)
+
 
 def nest_page(driver):
-	'''处理嵌套页面的情况：页面内又含有分支页面'''
-	global Depth
-	if Depth<=1: #深度最多为1层
-		try:
-			driver.find_element_by_class_name('pro_list_pic')
-		except NoSuchElementException:
-			Depth+=1
-			print 'need to jump again'
-			eles=driver.find_elements_by_tag_name('a')
-			pattern=re.compile('show-\d+.html$')
-			for ele in eles:
-				if pattern.search(str(ele.get_attribute('href'))):
-					print 'before click'
-					action=ActionChains(driver)
-					action.click(ele).perform()
-					print 'after click'
-					# 切换到新的窗口
-					base_page=ele.get_attribute('href')
-					print 'go into '+ base_page
-					try:
-						driver.current_url
-					except:
-						pass
-					page.switch_do(driver,process,(base_page,))
-			Depth-=1
-			return 
-
-def process(driver,base_page):
-	'''处理每一个品牌'''
-	nest_page(driver) #嵌套页面
-
-	sizeIdx=-1
-	for type,size in zip(Type,Size):
-		print type,size
-		has_more=True
-		num=1
-		sizeIdx+=1
-		while has_more:
-			# 加载新页
-			new_url=base_page+"?q=0|%s|%s|%s|%s|%d"%(TYPE[type],SIZE[size.lower()],EMPTY[Empty],SORT[Sort],num)
-			print new_url
-			driver.set_page_load_timeout(3) #3s超时
-			page.call_ignore(driver.get,new_url)
-			num+=1
-			# 判断是否存在
-			try: 
-				driver.find_element_by_class_name('pro_find_none')
-			except NoSuchElementException:
-				pass
-			else: 
-				print platfm.local_str('不存在该类别的商品')
-				break 
-			# 判断是否是最后一页
-			try:
-				attr=driver.find_element_by_class_name('page_next').get_attribute('href')
-				if not attr:
-					has_more=False
-			except:
-				has_more=False
-
-			control(driver,str(sizeIdx))
+    '''处理嵌套页面的情况：页面内又含有分支页面'''
+    print 'in nest_page'
+    global Depth
+    if Depth <= 1:  # 深度最多为1层
+        try:
+            driver.find_element_by_class_name('pro_list_pic')
+        except NoSuchElementException:
+            Depth += 1
+            print 'need to jump again'
+            eles = driver.find_elements_by_tag_name('a')
+            pattern = re.compile('show-\d+.html$')
+            for ele in eles:
+                if pattern.search(str(ele.get_attribute('href'))):
+                    print 'before click'
+                    action = ActionChains(driver)
+                    action.click(ele).perform()
+                    print 'after click'
+                    # 切换到新的窗口
+                    base_page = ele.get_attribute('href')
+                    print 'go into ' + base_page
+                    try:
+                        driver.current_url
+                    except:
+                        pass
+                    page.switch_do(driver, process, base_page)
+            Depth -= 1
+            return
 
 
-def search_brand(item,name):
-	'''搜需要的品牌'''
-	try: #如果找不到，就直接return
-		lis=driver.find_elements_by_class_name(item)
-	except:
-		print platfm.local_str('没有'+item+'条目，返回')
-		return
-	#driver.set_page_load_timeout(0.5) #0.5s超时,太短的话(0.1s)click可能会超时
-	driver.set_page_load_timeout(4) #等待嵌套页面加载需要的时间较长
-	for li in lis:
-		info=li.find_element_by_class_name(name).text
-		print info
-		# 至少包括Brands里的一项，包括Keys里的所有项
-		if (filter(lambda x: x.lower() in info.lower(),Brands) and 
-			(Keys and Keys==filter(lambda x:x.lower() in info.lower(),Keys) or not Keys )):
-			if u"男女" in info:
-				MixFlag=True
-			print 'before click'
-			action=ActionChains(driver)
-			action.click(li).perform()
-			print 'after click'
-			time.sleep(0.5)
-			# 切换到新的窗口
-			base_page=''
-			try:
-				base_page=driver.current_url
-			except:
-				pass
-			print 'go into '+info+base_page
-			page.switch_do(driver,process,(base_page,))
-			MixFlag=False
+def process(driver, base_page):
+    '''处理每一个品牌'''
+    # 暂时不处理嵌套页面
+    # nest_page(driver)  # 嵌套页面
+    print 'process begins...'
+    sizeIdx = -1
+    for type, size in zip(Type, Size):
+        print type, size
+        has_more = True
+        num = 1
+        sizeIdx += 1
+        while has_more:
+            # 加载新页
+            new_url = base_page + \
+                "?q=0|%s|%s|%s|%s|%d" % (
+                    TYPE[type],
+                    SIZE[size.lower()],
+                    EMPTY[Empty],
+                    SORT[Sort],
+                    num)
+            print new_url
+            driver.set_page_load_timeout(3)  # 3s超时
+            page.call_ignore(driver.get, new_url)
+            num += 1
+            print 'after get the page'
+            # 判断是否存在
+            try:
+                driver.find_element_by_class_name('pro_find_none')
+            except NoSuchElementException:
+                pass
+            else:
+                print platfm.local_str('不存在该类别的商品')
+                break
+            # 判断是否是最后一页
+            try:
+                attr = driver.find_element_by_class_name(
+                    'page_next').get_attribute(
+                    'href')
+                if not attr:
+                    has_more = False
+            except:
+                has_more = False
+            print 'before control'
+            control(driver, str(sizeIdx))
+            print 'after control'
+    print 'process ends...'
 
 
-
-if __name__=='__main__':
-	try:
-		platfm.init()
-		proc.forks()
-		print 'after forks'
-	
-		driver=webdriver.Chrome()
-		driver.set_window_position(0,0)
-		driver.set_window_size(ScreenW,ScreenH1)
-
-		platfm.first_timeout(driver)
-		page.call_ignore(driver.get,HomeUrl)
-
-		if TestMode:
-			# 如果不执行wait_for_begin，就必须刷新一次，否则选地址的div挡着没法点击链接
-			print 'TestMode'
-			# ele=WebDriverWait(driver,60,2,(Exception))
-			# .until(lambda x:x.find_element_by_class_name('sdf_item'))
-			driver.set_page_load_timeout(40)#40s超时,等待第一次加载
-			page.call_ignore(driver.refresh)
-		else:
-			print 'Non-TestMode'
-			page.wait_for_begin(driver)
-
-		print 'after wait_for_begin'
-
-		# 展示区
-		search_brand('sdf_item','sdf_name') #class='sdf_item sdf_item_only'也包括在内
-		# 今日新品
-		search_brand('s_mod','s_info_name')
-	except Exception as e:
-		print e
-	finally:
-		driver.quit()
-		proc.inform_subp()
-		platfm.uninit()
+def search_brand(item, name):
+    '''搜需要的品牌'''
+    print 'in search_brand',item,name
+    try:  # 如果找不到，就直接return
+        lis = driver.find_elements_by_class_name(item)
+    except:
+        print platfm.local_str('没有' + item + '条目，返回')
+        return
+    print 'after find'
+    # driver.set_page_load_timeout(0.5) #0.5s超时,太短的话(0.1s)click可能会超时
+    driver.set_page_load_timeout(4)  # 等待嵌套页面加载需要的时间较长
+    for li in lis:
+        info = li.find_element_by_class_name(name).text
+        print info
+        # 至少包括Brands里的一项，包括Keys里的所有项
+        if (filter(lambda x: x.lower() in info.lower(), Brands) and
+                (Keys and Keys == filter(lambda x: x.lower() in info.lower(),
+                 Keys) or not Keys)):
+            global MixFlag
+            if u"男女" in info:
+                MixFlag = True
+            action = ActionChains(driver)
+            action.click(li).perform()
+            print 'after click'
+            time.sleep(5)
+            print 'after sleep'
+            # 切换到新的窗口
+            print 'go into ' + info
+            page.switch_do(driver, process)
+            MixFlag = False
 
 
+if __name__ == '__main__':
+    try:
+        platfm.init()
+        proc.forks()
+        print 'after forks'
+
+        driver = webdriver.Chrome()
+        driver.set_window_position(0, 0)
+        driver.set_window_size(ScreenW, ScreenH1)
+
+        # platfm.first_timeout(driver)
+        page.call_ignore(driver.get, HomeUrl)
+        print 'after get the homepage'
+
+        if TestMode:
+            # 如果不执行wait_for_begin，就必须刷新一次，否则选地址的div挡着没法点击链接
+            print 'TestMode'
+            xpath="//div[@class='area_choose']/dl[3]/dd/a[7]"
+            ele=WebDriverWait(driver,30).until(EC.element_to_be_clickable((By.XPATH,xpath)))
+            print 'before click'
+            ele.click() # 选择地区
+        else:
+            print 'Non-TestMode'
+            page.wait_for_begin(driver)
+            print 'after wait_for_begin'
+
+        # stop=raw_input('cont?')
+
+        # 展示区
+        # class='sdf_item sdf_item_only'也包括在内
+        search_brand('sdf_item', 'sdf_name')
+        # 今日新品
+        search_brand('s_mod', 's_name')
+    except Exception as e:
+        print e
+    finally:
+        a=raw_input('input:')
+        driver.quit()
+        print 'driver quited'
+        proc.inform_subp()
+        print 'sended q to subps'
+        platfm.uninit()
--- slave.py
+++ slave.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from __future__ import print_function #必须要出现在文件开头
+from __future__ import print_function  # 必须要出现在文件开头
 import time
 import sys
 import re
@@ -20,134 +20,152 @@ from selenium.webdriver.common.by import By
 
 from config import *
 
-MaxReload=2 #reload的最大次数
-HasGood=False
-TimeoutCnt=0 #超时计数
+MaxReload = 2  # reload的最大次数
+HasGood = False
+TimeoutCnt = 0  # 超时计数
+
 
 def p_print(*args):
-	'''带有pid的输出'''
-	print(os.getpid(),':',end='')
-	for arg in args:
-		print(arg,end=' ')
-	print('\n',end='')
+    '''带有pid的输出'''
+    print(os.getpid(), ':', end='')
+    for arg in args:
+        print(arg, end=' ')
+    print('\n', end='')
 
 
-def buy(driver,size,pri_block):
-	"购买对应尺码的商品"
-	p_print('buy')
-	SizeReg=''
-	if size!=u'默认':
-		SizeReg=u'^'+size+u'([^.L]|$)'
-	else: #如果是默认尺码，那就所有商品都选
-		SizeReg=u'.*'
-	cnt=0
-	ref=False
-#    while True: 
-#		try:
-#	if ref:
-#		driver.refresh()
-	# 定位出价格显示区
-	# pri_block=driver.find_element_by_class_name("size_list")
-	btn=driver.find_element_by_id('J_cartAdd_submit')
-	for li in pri_block.find_elements_by_tag_name('li'):
-		# 没有售完，且大小符合要求
-		class_attr=li.get_attribute('class')
-		if class_attr and 'sli_disabled' in class_attr: #售完的
-			continue
+def buy(driver, size):
+    "购买对应尺码的商品"
+    p_print('buy')
+    SizeReg = ''
+    if size != u'默认':
+        SizeReg = u'^' + size + u'([^.L]|$)'
+    else:  # 如果是默认尺码，那就所有商品都选
+        SizeReg = u'.*'
+    cnt = 0
+    ref = False
+    while True:
+        try:
+            if ref:
+                driver.refresh()
+            # 定位出价格显示区
+            pri_block=driver.find_element_by_class_name("size_list")
+            btn = driver.find_element_by_id('J_cartAdd_submit')
+            for li in pri_block.find_elements_by_tag_name('li'):
+                # 没有售完，且大小符合要求
+                class_attr = li.get_attribute('class')
+                if class_attr and 'sli_disabled' in class_attr:  # 售完的
+                    continue
 
-		span=li.find_element_by_tag_name('span')
-		if re.search(SizeReg,span.text.strip('" ')):
-			action=ActionChains(driver)
-			action.click(li).click(btn).perform() # ！！！ click会超时，至少2s的page_load_timeout
-			p_print('got size',span.text.strip())
-			global HasGood #!!!全局变量
-			HasGood=True
-			time.sleep(1)
-#		except TimeoutException:
-#			p_print('timeout')
-#			ref=False
-#		except Exception as e:
-#			p_print(e)
-#			global TimeoutCnt
-#			TimeoutCnt+=1
-#			if cnt==MaxReload:
-#				p_print('get to MaxReload,return')
-#				return #直接函数返回，忽略该商品
-#			else:
-#				cnt+=1
-#				cur=(cnt+1)*BuyTimeout
-#				driver.set_page_load_timeout(cur) #每次超时时间，延长BuyTimeout
-#				p_print(cur)
-#				ref=True#重新加载该页
-#		else:
-#			break
+                span = li.find_element_by_tag_name('span')
+                print("test",span.text)
+                if re.search(SizeReg, span.text.strip('" ')):
+                    action = ActionChains(driver)
+                    # ！！！ click会超时，至少2s的page_load_timeout
+                    action.click(li).click(btn).perform()
+                    p_print('got size', span.text.strip())
+                    global HasGood  # !!!全局变量
+                    HasGood = True
+                    time.sleep(1)
+        except TimeoutException:
+            p_print('timeout')
+            ref=False
+        except Exception as e:
+            p_print(e)
+            global TimeoutCnt
+            TimeoutCnt+=1
+            if cnt==MaxReload:
+                p_print('get to MaxReload,return')
+                return #直接函数返回，忽略该商品
+            else:
+                cnt+=1
+                cur=(cnt+1)*BuyTimeout
+                driver.set_page_load_timeout(cur) #每次超时时间，延长BuyTimeout
+                p_print(cur)
+                ref=True#重新加载该页
+        else:
+            break
 
-def login(driver,user,pwd):
-	driver.get(LoginUrl)
-	driver.find_element_by_id("J_L_name").send_keys(user)
-	driver.find_element_by_id('J_L_psw').send_keys(pwd)
-	driver.find_element_by_id("user_login_form").submit()
-	driver.back()#!!!
-	
 
-if __name__=='__main__':
+def login(driver, user, pwd):
+    driver.get(LoginUrl)
+    driver.find_element_by_id("J_L_name").send_keys(user)
+    driver.find_element_by_id('J_L_psw').send_keys(pwd)
+    driver.find_element_by_id("user_login_form").submit()
+    driver.back()  # !!!
 
-	p_print('slave')
-	user,pwd,xpos=sys.argv[1],sys.argv[2],sys.argv[3]
-	p_print(user,pwd)
-	while True: #防止调用Chrome出错
-		try:
-			driver=webdriver.Chrome()
-		except:
-			time.sleep(2)
-		else:
-			break
-	
-	driver.set_window_size(ScreenW/len(Account),ScreenH2)
-	driver.set_window_position(int(xpos),ScreenH1)
-	login(driver,user,pwd)
 
-	try:
-		driver.set_page_load_timeout(BuyTimeout) # 从配置文件读取timeout值
-		while True:
-			infos=sys.stdin.readline() #!!!read()不行，是全缓冲的
-			# 退出
-			if infos.find('q')==0: # 开头是q的，表示退出
-				if TimeoutCnt>=2:  # 发生两次超时
-					timeout_inc(0.5)
-				if HasGood:
-					p_print('has goods')
-					driver.maximize_window() # 最大化有物品的窗口
-					# 停止
-					while True:
-						time.sleep(10000)
-				else:
-					p_print('has no goods')
-					break
-			href=infos.split()[0]
-			sizeIdx=int(infos.split()[1])
-			size=Size[sizeIdx]
-			p_print(href,size)
-			cpid=os.fork()
-			if cpid==0:
-				driver.get(href)
-				os._exit(0)
+if __name__ == '__main__':
 
-			try:
-				pri_block = WebDriverWait(driver, 20).until(
-					EC.presence_of_element_located((By.CLASS_NAME,'size_list')))
-				# kill the child process
-				os.kill(cpid,sinal.SIG_KILL)
-			except:
-				pass
-			else:
-				buy(driver,size,pri_block)
-#			try:
-#				driver.get(href)
-#			except TimeoutException:
-#				pass
-	except Exception as e:
-		p_print('error:',e)
-	finally:
-		driver.quit()
+    p_print('slave')
+    user, pwd, xpos = sys.argv[1], sys.argv[2], sys.argv[3]
+    p_print(user, pwd)
+    while True:  # 防止调用Chrome出错
+        try:
+            driver = webdriver.Chrome()
+        except:
+            time.sleep(2)
+        else:
+            break
 
+    driver.set_window_size(ScreenW / len(Account), ScreenH2)
+    driver.set_window_position(int(xpos), ScreenH1)
+    login(driver, user, pwd)
+
+    try:
+        driver.set_page_load_timeout(BuyTimeout)  # 从配置文件读取timeout值
+        while True:
+            infos = sys.stdin.readline()  # !!!read()不行，是全缓冲的
+            # 退出
+            if infos.find('q') == 0:  # 开头是q的，表示退出
+                if TimeoutCnt >= 2:  # 发生两次超时
+                    timeout_inc(0.5)
+                if HasGood:
+                    p_print('has goods')
+                    driver.maximize_window()  # 最大化有物品的窗口
+                    # 停止
+                    while True:
+                        signal.pause()
+                else:
+                    p_print('has no goods')
+                    break
+            href = infos.split()[0]
+            sizeIdx = int(infos.split()[1])
+            size = Size[sizeIdx]
+            p_print(href, size)
+#            cpid = os.fork()
+#            if cpid == 0:
+#                p_print('child: before get')
+#                driver.get(href)
+#                p_print('child: after get')
+#                os._exit(0)
+#
+#            try:
+#                p_print('parent: before until')
+#                pri_block = WebDriverWait(driver, 5).until(
+#                    EC.presence_of_element_located((By.CLASS_NAME, 'size_list')))
+#                btn = WebDriverWait(driver, 5).until(
+#                    EC.element_to_be_clickable((By.CLASS_NAME, 'J_cartAdd_submit')))
+#                p_print('parent: after until')
+#                # kill the child process
+#                os.kill(cpid, signal.SIGKILL)
+#                p_print('parent: after kill')
+#                status = os.waitpid(cpid, os.WNOHANG)[1]
+#                if os.WIFSIGNALED(status):
+#                    p_print('term signal:', os.WTERMSIG(status))
+#                elif os.WIFEXITED(status):
+#                    p_print('exit code:', os.WEXITSTATUS(status))
+#                else:
+#                    p_print('unknown waitstatus')
+#            except Exception as e:
+#                p_print('parent error:',e)
+#            else:
+#                buy(driver, size, pri_block)
+            try:
+                driver.get(href)
+            except TimeoutException:
+                pass
+            buy(driver, size)
+    except Exception as e:
+        p_print('error:', e)
+    finally:
+        driver.quit()
